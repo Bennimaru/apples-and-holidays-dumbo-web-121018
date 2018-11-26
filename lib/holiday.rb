07370d41740e7e6935c8c3d7ab6a35e8e2796ba0
Benson Yu <bennywhyu@gmail.com>
2018-11-26 22:44:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,9 +57,9 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].collect do |holiday,supplies|
     winter_season_supplies.push(supplies)
   end
+  
   winter_season_supplies.flatten
   
-
 end
 
 def all_supplies_in_holidays(holiday_hash)
